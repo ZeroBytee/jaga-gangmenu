fd9ec235f290bd58d0348d9937dffef349c69381
Galaxic <96119080+ZeroBytee@users.noreply.github.com>
2023-09-22 21:27:35 +0200

    idkman

--- client/client.lua
+++ client/client.lua
@@ -584,16 +584,3 @@ end)
 
 RegisterKeyMapping('+gangmenu', 'Open gang menu', 'keyboard', Config.jobMenu)
 
-
--- prevent accidentally hitting someone
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        DisableControlAction(1, 140, true)
-        if not IsPlayerTargettingAnything(PlayerId()) then
-            DisableControlAction(1, 141, true)
-            DisableControlAction(1, 142, true)
-        end
-    end
-end)
-
